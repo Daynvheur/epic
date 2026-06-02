2716d6930c91a8b364de843e51e0368c676bd7d5
EXT-Gweltaz <ext.g.lemartret@lacroix.group>
2026-06-02 11:58:47 +0200

    modernized compile.bat Plus displayed REM example

--- compile.bat
+++ compile.bat
@@ -1,5 +1,8 @@
 cd /d %~dp0
 @REM npm install
 @REM npm audit fix
-@REM npx browserslist@latest --update-db
+@REM npm approve-scripts -a
+@REM npm install -g parcel-bundler
+@REM npx update-browserslist-db@latest
+REM example url: http://localhost:1234/?range=1252&complexity=42&circles=1&pt=100;500&pt=100;100&pt=500;100&pt=100;500&pt=500;500&pt=100;100&pt=300;0&pt=500;100&pt=500;500
 parcel serve src/index.html
